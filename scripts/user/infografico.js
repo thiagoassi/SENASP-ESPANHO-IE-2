27011be612af79395a6f9cdceda9539c5306c2c0
Renan Pinho Assi <renanpinhoassi@gmail.com>
2020-02-26 15:33:58 -0300

    Major updates

--- scripts/user/infografico.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById("infograficoAnimado"),
-  renderer: "svg",
-  loop: false,
-  autoplay: true,
-  path: "assets/images/module_1/infografico/infografico_1.json"
-});
